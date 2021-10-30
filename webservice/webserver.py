3bac8fe62ab16f48a135682f591a10ffa74b4d14
Juan Bobadilla <juan.bobadilla01@correo.usa.edu.co>
2021-10-29 21:00:05 -0500

    interfaz aplicación monolítica

--- webservice/webserver.py
+++ webservice/webserver.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import requests
 import os
+from flask import render_template
 
 app=Flask(__name__)
 
@@ -9,16 +10,32 @@ ip_ai_server = os.getenv("IA_IP") or "127.0.0.1"
 
 @app.route("/credit/predict", methods=['POST'])
 def predict():
-    content = request.get_json()
-    age = content['age']
-    housing = content['housing']
-    credit_amount = content['credit_amount'] 
+    name = request.form.get('nombre')
+    age = int(request.form.get('age'))
+    housing = request.form.get('housing')
+    credit_amount = int(request.form.get('credit_amount'))
 
     response = requests.post(f"http://{ip_ai_server}:5000/servicioia", 
                         json={"age": age, "housing": housing,"credit_amount": credit_amount})
-    print("ia responde", response.json())
-    return {"prediction": response.json()}
+    
 
+    response = response.json()
+    print(response)
+    good_score_proba = response['prediction']['good_score_proba']
+    bad_score_proba = response['prediction']['bad_score_proba']
+    if good_score_proba >=0.5:
+       good_client = True
+       proba = good_score_proba
+    else:
+       good_client = False
+       proba = bad_score_proba
+
+    return render_template("response.html", nombre= name, credit_amount= credit_amount, proba= proba, good_client= good_client)
+
+
+@app.route('/')
+def form():
+    return render_template("form.html")
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=8080)
