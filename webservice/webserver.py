ee3a9ca5041bada5d5bfa1853fc99b8956967864
Juan Bobadilla <juan.bobadilla01@correo.usa.edu.co>
2021-11-05 18:31:17 -0500

    one page finalizada

--- webservice/webserver.py
+++ webservice/webserver.py
@@ -3,8 +3,10 @@ from flask import request
 import requests
 import os
 from flask import render_template
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 
 ip_ai_server = os.getenv("IA_IP") or "127.0.0.1"
 
@@ -31,6 +33,32 @@ def predict():
        proba = bad_score_proba
 
     return render_template("response.html", nombre= name, credit_amount= credit_amount, proba= proba, good_client= good_client)
+
+@app.route("/credit/predict/v2",methods=["POST"])
+def predict2():
+    content = request.get_json()
+    name = content['nombre']
+    age = int(content['age'])
+    housing = content['housing']
+    credit_amount = int(content['credit_amount'])
+
+    response = requests.post(f"http://{ip_ai_server}:5000/servicioia", 
+                        json={"age": age, "housing": housing,"credit_amount": credit_amount})
+    
+
+    response = response.json()
+    print(response)
+    good_score_proba = response['prediction']['good_score_proba']
+    bad_score_proba = response['prediction']['bad_score_proba']
+    if good_score_proba >=0.5:
+       good_client = True
+       proba = good_score_proba
+    else:
+       good_client = False
+       proba = bad_score_proba
+
+    return {"nombre": name, "credit_amount":credit_amount, "proba":proba, "good_client":good_client}
+
 
 
 @app.route('/')
